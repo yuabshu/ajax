a82955853b1752d87ff3528ffa29790d0f5cb142
yuabshu <yuabshu@163.com>
2017-04-06 18:58:07 +0800

    use jquery

--- ajax.js
+++ ajax.js
@@ -4,3 +4,40 @@
 	3.然后改变ajax获取图片的路径，可以专门写一个生成路径的方法；
 	4点击上一页或下一页时就调用相应的方法来来生成路径，再将路径用到ajax的方法中；
 	5.此时就可以改变获取的图片内容了*/
+$(function(){
+	/*function addimg(){//纯粹利用jquery方法实现翻页
+		var i=1;var str=null;
+		$('.next').removeAttr("disabled");
+		$('.last').removeAttr("disabled");
+		$(".next").click(function(){
+			$('.last').removeAttr("disabled");
+			if(i<5){
+				++i;
+				$('.next').removeAttr("disabled");
+				str='<img src="img/'+i+'.jpg">';
+				$("#main").html();
+				$("#main").html(str);				
+			}else{
+				$('.next').attr('disabled',"true");
+			}		
+		})
+		$(".last").click(function(){			
+			if(i>=2){
+				i--;
+				$('.next').removeAttr("disabled");
+				str='<img src="img/'+i+'.jpg">';
+				$("#main").html(str);	
+			}else{
+				$('.last').attr('disabled',"true");
+			}
+			$('.next').removeAttr("disabled");
+		})
+	}*/
+
+
+	//function addajax(){//利用ajax技术实现翻页
+		$(".next").click(function(){
+			$("#main").load("data.html");
+		})
+	//}
+})
